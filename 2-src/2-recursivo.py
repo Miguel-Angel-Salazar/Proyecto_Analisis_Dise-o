085bed41dd6daff469788ed87dc9b7c7d770597f
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2026-05-16 23:15:57 +0000

    metodo_recursivo

---
+++ 2-src/2-recursivo.py
@@ -0,0 +1,109 @@
+from lector_datos import cargar_datos
+
+
+def hay_conflicto(asignaciones, profesor, grupo, salon, horario):
+
+    for a in asignaciones:
+
+        if a["salon"] == salon and a["horario"] == horario:
+            return True
+
+        if a["profesor"] == profesor and a["horario"] == horario:
+            return True
+
+        if a["grupo"] == grupo and a["horario"] == horario:
+            return True
+
+    return False
+
+
+def resolver_recursivo(
+    materias,
+    salones,
+    horarios,
+    indice,
+    asignaciones
+):
+
+    # caso base
+    if indice >= len(materias):
+        return
+
+    materia = materias[indice]
+
+    for salon in salones:
+
+        if salon["capacidad"] >= materia["alumnos"]:
+
+            for horario in horarios:
+
+                conflicto = hay_conflicto(
+                    asignaciones,
+                    materia["profesor"],
+                    materia["grupo"],
+                    salon["nombre"],
+                    horario
+                )
+
+                if conflicto == False:
+
+                    nueva = {
+                        "materia": materia["nombre"],
+                        "profesor": materia["profesor"],
+                        "grupo": materia["grupo"],
+                        "salon": salon["nombre"],
+                        "horario": horario
+                    }
+
+                    asignaciones.append(nueva)
+
+                    # llamada recursiva
+                    resolver_recursivo(
+                        materias,
+                        salones,
+                        horarios,
+                        indice + 1,
+                        asignaciones
+                    )
+
+                    return
+
+
+casos = [
+    "caso_prueba_pequeno.txt",
+    "caso_prueba_mediano.txt",
+    "caso_prueba_grande.txt"
+]
+
+
+for caso in casos:
+
+    print("\n===================================")
+    print("CASO:", caso)
+    print("===================================\n")
+
+    materias, salones, horarios, restricciones, preferencias = cargar_datos(
+        caso
+    )
+
+    asignaciones = []
+
+    resolver_recursivo(
+        materias,
+        salones,
+        horarios,
+        0,
+        asignaciones
+    )
+
+    print("HORARIO GENERADO\n")
+
+    for r in asignaciones:
+
+        print("Materia:", r["materia"])
+        print("Profesor:", r["profesor"])
+        print("Grupo:", r["grupo"])
+        print("Salon:", r["salon"])
+        print("Horario:", r["horario"])
+
+        print("-----------------------")
