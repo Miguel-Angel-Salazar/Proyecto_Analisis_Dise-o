92000cd34a6be04c72479bb55a944b3edc8de9f3
Sebastián Villa <sebasviro567@gmail.com>
2026-05-17 22:10:55 +0000

    inicio_codigo_backtracking

--- 2-src/4-backtracking.py
+++ 2-src/4-backtracking.py
@@ -1,1 +1,19 @@
 
+from lector_datos import cargar_datos
+
+
+def hay_conflicto(asignaciones, profesor, grupo, salon, horario):
+
+    for a in asignaciones:
+
+        if a["salon"] == salon and a["horario"] == horario:
+            return True
+
+        if a["profesor"] == profesor and a["horario"] == horario:
+            return True
+
+        if a["grupo"] == grupo and a["horario"] == horario:
+            return True
+
+    return False
+
